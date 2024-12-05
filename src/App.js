50db185f93079cc25ba951490c091135fb1620ed
Adi Vrskic <adi_vrskic1@homedepot.com>
2024-12-05 15:23:21 -0500

    nice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,184 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { data } from './data/work';
+import { PiLinkedinLogoThin, PiGithubLogoThin, PiPaperPlaneTiltThin, PiArticleThin, PiArrowRightThin } from "react-icons/pi";
 
-function App() {
+import './index.scss';
+
+const App = () => {
+  const [activeSlide, setActiveSlide] = useState(null); // Active slide index
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    // Check screen size
+    const checkScreenSize = () => {
+      setIsSmallScreen(window.innerWidth < 1400);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href =
+      "https://panorama-slider.uiinitiative.com/assets/index.c1d53924.css";
+    document.head.appendChild(link);
+
+    const preloadLink = document.createElement("link");
+    preloadLink.rel = "modulepreload";
+    preloadLink.href =
+      "https://panorama-slider.uiinitiative.com/assets/vendor.dba6b2d2.js";
+    document.head.appendChild(preloadLink);
+
+    const script = document.createElement("script");
+    script.type = "module";
+    script.crossOrigin = "anonymous";
+    script.src =
+      "https://panorama-slider.uiinitiative.com/assets/index.d2ce9dca.js";
+    document.body.appendChild(script);
+
+    const observer = new MutationObserver(() => {
+      const activeBullet = document.querySelector(
+        ".swiper-pagination-bullet.swiper-pagination-bullet-active"
+      );
+
+      if (activeBullet) {
+        // Get the index and adjust it if necessary
+        const index = Array.from(
+          activeBullet.parentElement.children
+        ).indexOf(activeBullet);
+
+        setActiveSlide(index - 1 < 0 ? 7 : index - 1); // Ensure no negative indices
+      }
+    });
+
+    const pagination = document.querySelector(".swiper-pagination");
+    if (pagination) {
+      observer.observe(pagination, { attributes: true, subtree: true });
+    }
+
+    // Simulate loading animation
+    setTimeout(() => setIsLoading(false), 3000);
+
+    return () => {
+      document.head.removeChild(link);
+      document.head.removeChild(preloadLink);
+      document.body.removeChild(script);
+      observer.disconnect(); // Stop observing
+    };
+  }, []);
+
+  if (isSmallScreen) {
+    return (
+      <div className="under-construction">
+        <h1>Under Construction</h1>
+        <p>This site is best viewed on a desktop screen.</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading && (
+        <div className="loading-container">
+          <div className="loading-bar"></div>
+        </div>
+      )}
+      <div className="content">
+        <h1>Adi Vrskic</h1>
+
+        <div className="panorama-slider">
+          <div className="swiper">
+            <div className="swiper-wrapper">
+              {data?.map((item, i) => {
+                const isActive = activeSlide === i;
+
+                // Define dynamic box-shadow for active slide
+                const boxShadow = isActive
+                  ? `
+                    inset 0 0 25px ${item.color1 || 'rgba(255, 255, 255, 0.25)'}, 
+                    inset 10px 0 40px ${item.color2 || 'rgba(255, 0, 255, 0.25)'}, 
+                    inset -10px 0 40px ${item.color3 || 'rgba(0, 255, 255, 0.25)'}, 
+                    inset 10px 0 150px ${item.color2 || 'rgba(255, 0, 255, 0.25)'}, 
+                    inset -10px 0 150px ${item.color3 || 'rgba(0, 255, 255, 0.25)'}, 
+                    0 0 25px ${item.color1 || 'rgba(255, 255, 255, 0.25)'}, 
+                    -5px 0 40px ${item.color2 || 'rgba(255, 0, 255, 0.25)'}, 
+                    5px 0 40px ${item.color3 || 'rgba(0, 255, 255, 0.25)'}`
+                  : 'none';
+
+                return (
+                  <div
+                    key={i}
+                    className="swiper-slide"
+                    style={{
+                      boxShadow: boxShadow,
+                      transition: 'box-shadow 0.2s ease-in',
+                    }}
+                  >
+                    <img
+                      className="slide-image"
+                      src={item?.image}
+                      alt={item?.alt}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            <div className="swiper-pagination"></div>
+          </div>
+        </div>
+
+        <div className="slide-info">
+          {data[activeSlide] && 
+            <a className="slide-info-link" href={data[activeSlide]?.link} target="_blank" rel="noreferrer">
+              <h2>{data[activeSlide]?.name}</h2>
+              <PiArrowRightThin />
+            </a>
+          }
+          <div className="slide-info-skills">
+            {data[activeSlide] && data[activeSlide]?.skills?.map(skill => (
+              <span key={skill}>{skill}</span>
+            ))}
+          </div>
+        </div>
+
+        <div className="vignette"></div>
+
+        <div className="links">
+          <a href="mailto:adivrskic123@gmail.com" aria-label="adi vrskic's email">
+            <PiPaperPlaneTiltThin />
+          </a>
+          <a
+            href="https://github.com/adivrskic"
+            aria-label="adi vrskic's github"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <PiGithubLogoThin />
+          </a>
+          <a
+            href="https://linkedin.com/in/adi-vrskic"
+            aria-label="adi vrskic's linkedin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <PiLinkedinLogoThin />
+          </a>
+          <a
+            href="/Adi_Vrskic_Resume-1.pdf"
+            aria-label="adi vrskic's resume"
+            target="_blank"
+          >
+            <PiArticleThin />
+          </a>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/data/work.js
@@ -0,0 +1,82 @@
+export const data = [
+  {
+    id: 0,
+    name: 'The Home Depot',
+    image: '/images/homedepot.png',
+    link: 'https://www.homedepot.com',
+    skills: ['React', 'Tailwind', 'Frontend', 'Backend', 'GraphQL', 'Newrelic', 'Quantum Metric', 'Agile'],
+    color1: 'rgba(255, 255, 255, 0.5)', // Semi-transparent white
+    color2: 'rgba(252, 66, 12, 0.5)',  // Semi-transparent orange
+    color3: 'rgba(255, 179, 0, 0.5)'  // Semi-transparent yellow
+  },
+  {
+    id: 1,
+    name: 'Covered By Rugs',
+    image: '/images/coveredbyrugs.png',
+    link: 'https://www.coveredbyrugs.com',
+    skills: ['Javascript', 'Shopify', 'eCommerce', 'SEO', 'Google Analytics', 'Custom Development'],
+    color1: 'rgba(255, 255, 255, 0.5)', // Semi-transparent white
+    color2: 'rgba(94, 84, 48, 0.5)',    // Semi-transparent olive green
+    color3: 'rgba(118, 139, 68, 0.5)'   // Semi-transparent green
+  },
+  {
+    id: 2,
+    name: 'Mohawk Factory Outlet',
+    image: '/images/mohawkfactoryoutlet.png',
+    link: 'https://www.mohawkfactoryoutlet.com',
+    skills: ['Javascript', 'jQuery', 'Shopify', 'Tag Manager', 'SEO', 'Google Analytics', 'Custom Development'],
+    color1: 'rgba(255, 255, 255, 0.5)', // Semi-transparent white
+    color2: 'rgba(39, 39, 39, 0.5)',    // Semi-transparent dark grey
+    color3: 'rgba(255, 17, 0, 0.5)'    // Semi-transparent orange
+  },
+  {
+    id: 3,
+    name: 'Karastan Rugs',
+    image: '/images/karastan.png',
+    link: 'https://www.karastanrugs.com',
+    skills: ['Javascript', 'Shopify', 'eCommerce', 'SEO', 'Google Analytics', 'Custom Development'],
+    color1: 'rgba(255, 255, 255, 0.5)', // Semi-transparent white
+    color2: 'rgba(139, 102, 50, 0.5)',  // Semi-transparent brown
+    color3: 'rgba(179, 127, 66, 0.5)'   // Semi-transparent light brown
+  },
+  {
+    id: 4,
+    name: 'Battle & Brew',
+    image: '/images/battleandbrew.png',
+    link: 'https://www.battleandbrew.com',
+    skills: ['Squarespace', 'SEO', 'Google Analytics', 'Custom Development', 'Plugin Development'],
+    color1: 'rgba(255, 255, 255, 0.5)', // Semi-transparent white
+    color2: 'rgba(7, 7, 7, 0.5)',    // Semi-transparent dark grey
+    color3: 'rgba(214, 29, 82, 0.5)'    // Semi-transparent red
+  },
+  {
+    id: 5,
+    name: 'Pillow',
+    image: '/images/pillow.png',
+    link: 'https://unrivaled-longma-0a1d47.netlify.app/',
+    skills: ['React', 'Styled Components', 'Component Library', 'Npm'],
+    color1: 'rgba(255, 255, 255, 0.5)', // Semi-transparent white
+    color2: 'rgba(85, 99, 191, 0.5)',   // Semi-transparent blue
+    color3: 'rgba(252, 55, 255, 0.5)'   // Semi-transparent magenta
+  },
+  {
+    id: 6,
+    name: 'Copycat Plagiarism Analyzer',
+    image: '/images/copycat.png',
+    link: 'https://adivrskic.pythonanywhere.com',
+    skills: ['Python', 'Flask', 'Bulma', 'BeautifulSoup', 'Scikit-learn'],
+    color1: 'rgba(255, 255, 255, 0.5)', // Semi-transparent white
+    color2: 'rgba(41, 255, 185, 0.5)',  // Semi-transparent blue
+    color3: 'rgba(142, 68, 173, 0.5)'   // Semi-transparent purple
+  },
+  {
+    id: 7,
+    name: 'Infinite Zoom Fader',
+    image: '/images/izf.png',
+    link: 'https://scintillating-malasada-3227fb.netlify.app/',
+    skills: ['React', 'Npm', 'Reusable component', 'Customizable'],
+    color1: 'rgba(255, 255, 255, 0.5)', // Semi-transparent white
+    color2: 'rgba(48, 63, 159, 0.5)',   // Semi-transparent indigo
+    color3: 'rgba(103, 58, 183, 0.5)'   // Semi-transparent purple
+  }
+];
